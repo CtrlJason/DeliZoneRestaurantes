49517d40b9c268eaf8b02afb6a6f4e43b0136294
Yeison Mosquera <142964641+CtrlJason@users.noreply.github.com>
2024-09-13 14:36:36 -0500

    Reestructura

--- web/productos/urls.py
+++ web/productos/urls.py
@@ -6,8 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('crear_productos/', views.crear_producto, name='crear_productos'),
-    path('productos/', views.productos, name="productos"),
+    path('', views.productos, name="productos"),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
