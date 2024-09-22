2af64b9e2eba92abd79e2c3df3f72dfa2e59c402
Yeison Mosquera <142964641+CtrlJason@users.noreply.github.com>
2024-09-22 00:06:44 -0500

    Barra de navegacion
    - Se arreglo la barra de navegación y se le incorporo Bootstrap
    - Se agrego un desplegable para celulares
    - Se ajustaron los iconos de la barra de navegación

--- web/foodPartnerApp/static/js/carrito.js
+++ web/foodPartnerApp/static/js/carrito.js
@@ -1,29 +1,5 @@
-const btnCarrito = document.getElementById('btnCarrito');
-const modalCarrito = document.getElementById('modalCarrito');
-const span = document.getElementsByClassName('close')[0];
 const contadorCarrito = document.querySelector('.contador-productos');
 let contador = 0;
-
-// Función para abrir/cerrar el carrito
-function toggleCarrito() {
-    const displayStyle = window.getComputedStyle(modalCarrito).display;
-    if (displayStyle == "none") {
-        modalCarrito.style.display = "block";
-    } else {
-        modalCarrito.style.display = "none";
-    }
-}
-
-// Evento para el botón que abre/cierra el carrito
-btnCarrito.onclick = toggleCarrito;
-
-// Evento para cerrar el carrito con la 'x'
-span.onclick = function () {
-    modalCarrito.style.display = "none";
-}
-
-// Evento para abrir/cerrar el carrito cuando se haga clic en el número del contador
-document.querySelector('.contador-productos').onclick = toggleCarrito;
 
 // Selecciona todos los botones con la clase 'bttn'
 const botones = document.querySelectorAll('.bttn.incrementar');
